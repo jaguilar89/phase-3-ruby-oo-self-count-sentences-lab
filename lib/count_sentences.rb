9c375200ba6fbd846a42201980c4e037e0d81030
Jose Aguilar <jaguilarny@gmail.com>
2022-09-03 21:45:46 -0400

    commit tree 3d770dd292d509863a44e9ca266d5ae8f8feb993

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,19 @@
-require 'pry'
-
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    regex = /[.?!]+/
+    self.split(regex).count
   end
 end
